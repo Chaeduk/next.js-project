bf1efad421b04c6d6588f0fca2b2de8ef18e6395
Chaeduk <kkj2551@naver.com>
2022-09-11 20:46:59 +0900

    [feat] airbnb 클론 프로젝트 진행중(1)
    공통 헤더 컴포넌트 작업

---
+++ 03/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "../components/Header";
+
+const Container = styled.div``;
+
+const index: React.FC = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
+
+export default index;
